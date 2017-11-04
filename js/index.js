1ea2ff0da6c0101ab586886bc9829881d497947e
wendiang <371174590@qq.com>
2017-11-04 15:40:09 +0800

    add

--- js/index.js
+++ js/index.js
@@ -1,26 +1,53 @@
-window.onload=function(){
-  function $(dom){
-  
-  var dom=document.querySelectorAll(dom)
-	  for(var i=0;i<dom.length;i++){
-	  return dom[i];
+var content=document.querySelector(".content");
+  var item=content.getElementsByTagName("*");
+  var boxArr= new Array();
+   for(var i=0;i<item.length;i++){
+   if(item[i].className=="item"){
+   
+     boxArr.push(item[i]);
+   
+   }
+
+   
+   
+   
+   }
+   var hArr=new Array();
+   var minh;
+   for(var i=0;i<boxArr.length;i++){
+      if(i<2){
+      
+      hArr.push(boxArr[i].offsetHeight)
+      
+      }else{
+             minh=Math.min.apply(null,hArr);
+	      var index=getindex(hArr,minh)
+	      
+             boxArr[i].style.position="absolute";
+	      boxArr[i].style.top=minh+"px";
+	      boxArr[i].style.left=boxArr[index].offsetLeft+"px";
+	      
+              
+             hArr[index]+=boxArr[i].offsetHeight;
 
 	  
-	  
-	  }
-  
-  
-  }
 
- function on(even,type,fn){
-     even.addEventListener(type,fn,false);
+      
+      }
+   
+   
+   }
+
+ function getindex(arr,minh){
+ 
+   for(var i in arr){
+   if(arr[i]==minh){
+   return i;
+   
+   }
+   
+   }
  
  
- }
-
-
-   on($(".btn"),"click",function(){alert(4444)})
-
-}
-
+ } 
 
