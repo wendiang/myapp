592312ee79cc69dba8460aac6a3b3616f6ffe242
wendiang <371174590@qq.com>
2017-11-04 15:51:58 +0800

    add

--- js/index.js
+++ js/index.js
@@ -51,3 +51,21 @@ var content=document.querySelector(".content");
  
  } 
 
+var btn=document.querySelector(".btn")
+var bar=document.querySelector(".bar")
+bar.style.display="none";
+btn.onclick=function(){
+
+
+  
+   if(bar.style.display=="none"){
+         bar.style.display="block"
+   
+   }else if(bar.style.display=="block"){
+         bar.style.display="none"
+   }
+
+
+
+}
+
