e1c18314659f67bb77f3ff493f9bdec6a85be8a3
wendiang <371174590@qq.com>
2017-11-05 23:10:12 +0800

    add

--- js/index.js
+++ js/index.js
@@ -1,71 +1,78 @@
-var content=document.querySelector(".content");
-  var item=content.getElementsByTagName("*");
-  var boxArr= new Array();
-   for(var i=0;i<item.length;i++){
-   if(item[i].className=="item"){
-   
-     boxArr.push(item[i]);
-   
-   }
+(function(){
+  var App=function(content){
+     this.content=document.querySelector(content||".content")
+     this.item=this.content.getElementsByTagName("*")
+     
+     this.boxArr=new Array()
+     this.box=this.getbox(this.item);
+     this.hArr=new Array()
+     this.minh=0;
+     this.index=0;
 
-   
-   
-   
-   }
-   var hArr=new Array();
-   var minh;
-   for(var i=0;i<boxArr.length;i++){
-      if(i<2){
-      
-      hArr.push(boxArr[i].offsetHeight)
-      
-      }else{
-             minh=Math.min.apply(null,hArr);
-	      var index=getindex(hArr,minh)
-	      
-             boxArr[i].style.position="absolute";
-	      boxArr[i].style.top=minh+"px";
-	      boxArr[i].style.left=boxArr[index].offsetLeft+"px";
-	      
-              
-             hArr[index]+=boxArr[i].offsetHeight;
+     for(var i=0;i<this.box.length;i++){
+          if(i<2){
+          	this.hArr.push(this.box[i].offsetHeight)
+          }else{
+          	this.minh=Math.min.apply(null,this.hArr)
+            this.index=this.getindex(this.hArr,this.minh)
+            this.box[i].style.position="absolute"
+            this.box[i].style.top=this.minh+"px"
+            this.box[i].style.left=this.box[this.index].offsetLeft+"px"
+            this.hArr[this.index]+=this.box[i].offsetHeight;
+          }
+        
+     }
+     
+     
+  }
+  App.prototype.getbox=function(item){
+     for(var i=0;i<item.length;i++){
+     	if(item[i].className=="item"){
+     		this.boxArr.push(item[i])
+     	}
+     }
+      return this.boxArr;
+     
 
-	  
-
-      
-      }
-   
-   
-   }
-
- function getindex(arr,minh){
- 
-   for(var i in arr){
-   if(arr[i]==minh){
-   return i;
-   
-   }
-   
-   }
- 
- 
- } 
-
-var btn=document.querySelector(".btn")
-var bar=document.querySelector(".bar")
-bar.style.display="none";
-btn.onclick=function(){
+  }
+  App.prototype.getindex=function(arr,minh){
+     for(var i in arr){
+     	if(arr[i]==minh){
+     		return i;
+     	}
+     }
 
 
-  
-   if(bar.style.display=="none"){
-         bar.style.display="block"
-   
-   }else if(bar.style.display=="block"){
-         bar.style.display="none"
-   }
+  }
 
+var app=new App()
 
+var Btn=function(btn,bar){
+       this.btn=document.querySelector(btn||".btn")
+        var bar=document.querySelector(bar||".bar")
+
+        bar.style.display="none"
+        var self=this;
+      this.btn.addEventListener("click",fn)
+     function fn(){
+     	if(bar.style.display=="none"){
+     		self.open()
+     		bar.style.display="block"
+     	}else{
+     		self.close()
+     		bar.style.display="none"
+     	}
+     }   
 
 }
 
+Btn.prototype.open=function(){
+   console.log("打开")
+   
+}
+Btn.prototype.close=function(){
+   console.log("关闭")
+  
+}
+var btn=new Btn()
+})()
